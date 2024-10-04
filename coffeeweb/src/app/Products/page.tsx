5ed205a4b04f5f33accfbc6807e7556d9f607d51
Muhammad361738 <ullahehsan016@gmail.com>
2024-10-04 10:25:23 +0500

    first Nextjs website

--- coffeeweb/src/app/Products/page.tsx
+++ coffeeweb/src/app/Products/page.tsx
@@ -20,7 +20,7 @@ const Page = () => {
                          <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="w-64 h-58" src={card1} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>French Vanilla </div>
-                         <p className="text-xl font-bold text-black">French vanilla is a classic <br /> coffee flavor  that offers <br />a subtle and pleasing . <br /> Its aromatic touch enhances <br /> decaf blends and standard <br /> beverages packed <br /> with morning fuel. 
+                         <p className="text-xl font-bold text-black">French vanilla is a classic  coffee flavor  that offers a subtle and pleasing  Its aromatic touch enhances  decaf blends and standard beverages packed  with morning fuel
                          </p>
                      </div>
                      
@@ -28,7 +28,7 @@ const Page = () => {
                  <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="w-64 h-40" src={card2} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>Caramel</div>
-                         <p className="text-xl font-bold text-black">The candied and toasty palate of caramel complements any cup of coffee as an alternative to adding a spoonful of sugar. This flavor is gaining momentum because it offers a sweetness you can savor
+                         <p className="text-xl font-bold text-black">The candied and toasty palate of caramel complements any cup of coffee as an alternative to adding a spoonful of sugar This flavor is gaining momentum because it offers a sweetness you can savor
                          .</p>  
                      </div>
                  
@@ -36,7 +36,7 @@ const Page = () => {
                  <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="w-64 h-40" src={card3} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>Hazelnut </div>
-                         <p className="text-xl font-bold text-black">Considered a good choice almost anywhere, hazelnut flavoring provides a sweet and buttery taste. It emits a nutty aroma in freshly brewed batches. </p>
+                         <p className="text-xl font-bold text-black">Considered a good choice almost anywher hazelnut flavoring provides a sweet and buttery taste It emits a nutty aroma in freshly brewed batches </p>
                      </div>
 
                     
@@ -44,14 +44,14 @@ const Page = () => {
                          <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="w-64 h-40" src={card4} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'> Peppermint</div>
-                         <p className="text-xl font-bold text-black">Peppermint is becoming more common for brands because of its notable and refreshing taste. The minty fragrance works well in hot drinks as a seasonal delight in winter. </p>
+                         <p className="text-xl font-bold text-black">Peppermint is becoming more common for brands because of its notable and refreshing taste The minty fragrance works well in hot drinks as a seasonal delight in winter </p>
                      </div>
                     
                  {/* card5 */}
                  <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="w-64 h-40" src={card5} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>Mocha </div>
-                         <p className="text-xl font-bold text-black"> You cant go wrong with mocha, which is the perfect mix of richness and sweetness. It blends well with milk, chocolate, and coffee.
+                         <p className="text-xl font-bold text-black"> You cant go wrong with mocha which is the perfect mix of richness and sweetness It blends well with milk chocolate and coffee
                          </p>
                      </div>
                     
@@ -59,14 +59,14 @@ const Page = () => {
                  <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="w-64 h-40" src={card6} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>Amaretto </div>
-                         <p className="text-xl font-bold text-black">When you want the flavoring of an Italian liqueur without the alcohol, consider amaretto. The nutty aroma and taste of toasted almonds offer a sophisticated flair to many hot coffees. </p>
+                         <p className="text-xl font-bold text-black">When you want the flavoring of an Italian liqueur without the alcohol consider amaretto The nutty aroma and taste of toasted almonds offer a sophisticated flair to many hot coffees </p>
                      </div>
                     
                  {/* card7 */}
                  <div className="w-64 flex flex-col items-center mb-12 bg-amber-600  rounded-xl p-2">
                          <Image className="" src={card7} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>Black Coffie</div>
-                         <p className="text-xl font-bold text-black">Black coffee is simply coffee that is normally brewed without the addition of additives such as sugar, milk, cream, or added flavors. many people love a strong cup of black coffee.</p>
+                         <p className="text-xl font-bold text-black">Black coffee is simply coffee that is normally brewed without the addition of additives such as sugar milk cream or added flavors many people love a strong cup of black coffee</p>
                      </div>
                     
                  {/* card8 */}
@@ -74,7 +74,7 @@ const Page = () => {
                          <Image className="" src={card8} alt=''></Image>
                          <div className='text-2xl text-white font-mono mt-4'>Long Black.
                          </div>
-                         <p className="text-xl font-bold text-black">Moderate consumption of black coffee has been linked to a lower risk of heart disease and stroke. The antioxidants in coffee may help ...Black coffee is low in calories and can boost the metabolic rate, aiding in fat burning.</p>
+                         <p className="text-xl font-bold text-black">Moderate consumption of black coffee has been linked to a lower risk of heart disease and stroke The antioxidants in coffee may help Black coffee is low in calories and can boost the metabolic rate aiding in fat burning</p>
                        </div>           
                     </div>  
               </div>
