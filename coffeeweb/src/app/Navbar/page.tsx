893bfef793e15e06fb6e20b5c052c0504a5ad017
Muhammad361738 <ullahehsan016@gmail.com>
2024-10-06 23:36:17 +0500

    some changes

--- coffeeweb/src/app/Navbar/page.tsx
+++ coffeeweb/src/app/Navbar/page.tsx
@@ -6,15 +6,15 @@ const Navbar = () => {
     // const  [mobileFun.setMobile()]= useEffect()
     // setMobile()
     return (
-        <div className="flex justify-between bg-amber-900 h-16 pb-2 text-yellow-200 pl-5 w-screen fixed flex-wrap-reverse z-10 ">
+        <div className="flex justify-between bg-amber-700  h-16 pb-2 text-yellow-200 pl-5 w-screen fixed flex-wrap-reverse z-10 ">
             <Image className="sm:w-40 sm: rounded-xl h-12" src={logo} alt="" />
-            <div className="hidden md:block pt-3 ">
+            <div className="hidden md:block pt-3  ">
                 <ul className="outline-none  flex gap-12 pr-20 ">
 
-                    <li ><Link className="hover:bg-red-800" href={"/"}>Home</Link></li>
-                    <li><Link className="hover:bg-red-800" href={"/About"}>About</Link></li>
-                    <li><Link className="hover:bg-red-800" href={"/Products"}>Product</Link></li>
-                    <li><Link className="hover:bg-red-800" href={"/Contact"}>Contact</Link></li>
+                    <li ><Link className="hover:underline hover:bg-slate-700" href={"/"}>Home</Link></li>
+                    <li><Link className="hover:underline hover:bg-slate-700" href={"/About"}>About</Link></li>
+                    <li><Link className="hover:underline hover:bg-slate-700" href={"/Products"}>Product</Link></li>
+                    <li><Link className="hover:underline hover:bg-slate-700" href={"/Contact"}>Contact</Link></li>
                 </ul>
                            </div>
                            <div className="md:hidden  ">
